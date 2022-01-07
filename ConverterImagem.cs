b8a7e4cb764913bda2f8158c404daa778e4fffea
Roberto Silva <beto-frs@hotmail.com>
2022-01-07 03:12:45 -0300

    Initial Commit

---
+++ ConverterImagem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterIMG
+{
+    public class ConverterImagem
+    {
+        string Arquivo = @"D:\Users\beto-\Pictures\3x4.png";
+        public byte[] _imageArray { get; private set; }
+        public string _imageBase64 { get; private set; }
+
+        public string ext { get; private set; }
+
+        public void ConverterBase64()
+        {
+            ext = Arquivo.Split('.').Last();
+            byte[] ArrayByte = File.ReadAllBytes(Arquivo);
+            string ImageBase64 = Convert.ToBase64String(ArrayByte);
+
+            _imageArray = ArrayByte;
+            _imageBase64 = ImageBase64;
+
+            RestaurarImagem();
+
+        }
+
+        public void RestaurarImagem()
+        {
+            
+            byte[] bytes = Convert.FromBase64String(_imageBase64);
+            string arquivoConvertido = @$"{DateTime.Now.ToString("ddd-ss")}.{ext}";
+            File.WriteAllBytes(@$"E:\{arquivoConvertido}", bytes);
+            File.Copy(@$"E:\{arquivoConvertido}", @$"E:\CONSEGUI\{arquivoConvertido}");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConverterIMG
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ConverterImagem imagem = new ConverterImagem();
+            imagem.ConverterBase64();            
+        }
+
+    }
+}
